2044f08010180db8b2029ebdeafc923b32e43114
esempe <steam.dota.return@gmail.com>
2023-11-02 12:24:15 +0200

    added next app, tested shadcn, cleaned folders

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="">
+      <div className='text-3xl font-bold text-indigo-500'>
+        Hello discord clone
+      </div>
+      <Button>Button</Button>
+    </div>
+  )
+}
